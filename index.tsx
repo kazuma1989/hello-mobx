13293fc30d8d3099bf00e9d110182729cc16f68f
kazuma1989 <kazuma.ebina@quipper.com>
2019-11-01 12:05:33 +0900

    feat: Implement reactive example

--- index.tsx
+++ index.tsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+
+class Todo {
+  id = Math.random();
+  @observable title = "";
+  @observable finished = false;
+}
+
+const todo = new Todo();
+
+@observer
+class TodoViewerClassComp extends React.Component {
+  render() {
+    return <div>{todo.title}</div>;
+  }
+}
+
+const TodoViewerFuncComp = observer(function TodoViewerFuncComp() {
+  return <div>{todo.title}</div>;
+});
+
+function TodoInput() {
+  const [text, setText] = useState("");
+
+  return (
+    <input
+      value={text}
+      onChange={e => {
+        setText(e.target.value);
+        todo.title = e.target.value;
+      }}
+    />
+  );
+}
 
 function App() {
-  return <h1>HELLO</h1>;
+  return (
+    <div>
+      <h1>Hello MobX</h1>
+
+      <TodoInput />
+      <TodoViewerClassComp />
+      <TodoViewerFuncComp />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
