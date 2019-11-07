489bd34e44538ccb0cc2cd5ac5a1ba0eb2beff54
kazuma1989 <kazuma.ebina@quipper.com>
2019-11-07 11:49:02 +0900

    feat: Add a flow example

---
+++ index2.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { observable, runInAction, flow } from "mobx";
+import { observer } from "mobx-react";
+
+class ExampleStore {
+  @observable
+  message = "";
+
+  changeMessage = flow(this._changeMessage);
+  private *_changeMessage() {
+    this.message = "Good morning";
+
+    yield new Promise(resolve => {
+      setTimeout(() => {
+        this.message = "Good afternoon";
+        resolve();
+      }, 1000);
+    });
+
+    yield new Promise(resolve => {
+      setTimeout(() => {
+        this.message = "Good evening";
+        resolve();
+      }, 1000);
+    });
+  }
+}
+
+const store = new ExampleStore();
+
+const Greetings = observer(function Greetings() {
+  React.useEffect(() => {
+    store.message = "Good morning";
+
+    setTimeout(() => {
+      runInAction(() => {
+        store.message = "Good afternoon";
+
+        setTimeout(() => {
+          runInAction(() => {
+            store.message = "Good evening";
+          });
+        }, 1000);
+      });
+    }, 1000);
+  }, [store]);
+
+  return <div>{store.message}</div>;
+});
+
+const GreetingsWithFlow = observer(function GreetingsWithFlow() {
+  React.useEffect(() => {
+    store.changeMessage();
+  }, [store]);
+
+  return <div>{store.message}</div>;
+});
+
+function App() {
+  return (
+    <div>
+      <Greetings />
+      <GreetingsWithFlow />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
